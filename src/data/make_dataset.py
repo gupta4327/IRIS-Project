2c2deff47bbd1059eba0b6a7d19378ba434b3181
gupta4327 <gupta4327@gmail.com>
2023-12-03 11:11:27 +0530

    initialized project with a cookie cutter template

---
+++ src/data/make_dataset.py
@@ -0,0 +1,7 @@
+#importing pandas
+import pandas as pd
+
+#reading data
+iris_data = pd.read_csv(r'C:\Users\Aman Gupta\test\IRIS Project\data\raw\Iris.csv')
+
+
---
+++ src/features/build_features.py
@@ -0,0 +1,6 @@
+from src.data.make_dataset import iris_data
+import pandas as pd
+
+print(iris_data.head())
+
+
